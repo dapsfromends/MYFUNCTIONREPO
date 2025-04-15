e9d5be601078178c5b9df67b4441b9e0cf0f8668
dapsfromends <danieloladapo78@outlook.com>
2025-04-16 00:44:58 +0100

    time sleep

--- function_app.py
+++ function_app.py
@@ -2,6 +2,7 @@ import logging
 import json
 import os
 import uuid
+import time
 from datetime import datetime, timedelta
 
 import azure.functions as func
@@ -27,6 +28,7 @@ def get_table_client():
 @app.route(route="tasks", methods=["POST"], auth_level=func.AuthLevel.ANONYMOUS)
 def create_task(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("create_task function triggered")
+    time.sleep(5)
     try:
         data = json.loads(req.get_body())
         title = data.get("title")
@@ -60,6 +62,7 @@ def create_task(req: func.HttpRequest) -> func.HttpResponse:
 @app.route(route="tasks", methods=["GET"], auth_level=func.AuthLevel.ANONYMOUS)
 def get_tasks(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("get_tasks function triggered")
+    time.sleep(5)
     try:
         status_filter = req.params.get("status")
         table = get_table_client()
